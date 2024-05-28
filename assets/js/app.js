cbf4cb96d209d78649498fb524991ec6c6b84075
megantara007 <dikabhayu@gmail.com>
2024-05-28 17:13:31 +0700

    Final Version 1.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ let swiper = new Swiper('.swiper-container', {
     spaceBetween: 0,
     effect: 'fade',
     allowTouchMove: false,
-    initialSlide: 0,
+    initialSlide: 1,
     fadeEffect: {
         crossFade: true
     },
@@ -12,7 +12,7 @@ let swiper = new Swiper('.swiper-container', {
         clickable: true,
         renderBullet: function (index, className) {
             var icons = ['fa fa-qrcode', 'fa fa-puzzle-piece', 'fa fa-terminal', 'fa fa-file-image-o']; // Different texts for each button
-            var texts = ['Scanner', 'Custom QR', 'Checksum', 'Draw QRIS']; // Different texts for each button
+            var texts = ['Scanner', 'Customize', 'Checksum', 'Draw QRIS']; // Different texts for each button
             return '<span class="' + className + ' nav-btn"><i class="' + icons[index] + '"></i> ' + texts[index] + ' </span>';
         },
     },
--- assets/js/core.js
+++ assets/js/core.js
@@ -1,33 +1,269 @@
-const upload = document.getElementById('fileInput')
-if (upload) {
-    upload.addEventListener('change', function (event) {
-        const file = event.target.files[0];
-        const reader = new FileReader();
+document.cookie = "data-master=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+document.cookie = "data-client:length=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+document.cookie = "data-client:value=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+function qrReader(container) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-        reader.onload = function (event) {
-            const image = new Image();
-            image.onload = function () {
-                const canvas = document.createElement('canvas');
-                const context = canvas.getContext('2d');
-                canvas.width = image.width;
-                canvas.height = image.height;
-                context.drawImage(image, 0, 0, image.width, image.height);
+    reader.onload = function (event) {
+        const image = new Image();
+        image.onload = function () {
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0, image.width, image.height);
 
-                const imageData = context.getImageData(0, 0, image.width, image.height);
-                const code = jsQR(imageData.data, imageData.width, imageData.height);
-                if (!code) {
-                    document.getElementById('result').textContent = "No QR code found!"
-                    document.getElementById('copy-btn').classList.remove("hidden-copy-btn")
-                    close()
-                }
-                document.getElementById('result').textContent = code.data;
+            const imageData = context.getImageData(0, 0, image.width, image.height);
+            const code = jsQR(imageData.data, imageData.width, imageData.height);
+            if (!code) {
+                document.getElementById(container).textContent = "No QR code found!"
+
+                document.getElementById('copy-btn').classList.remove("show-copy-btn")
             }
-            image.src = event.target.result;
+            document.getElementById(container).textContent = code.data.toString().trim();
             document.getElementById('copy-btn').classList.add("show-copy-btn")
         }
+        image.src = event.target.result;
+    }
 
-        reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
+}
+
+function qrMergerReader() {
+    document.cookie = "data-master=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+
+    // document.getElementById("client-qr-container").style.display = "none"
+    // document.getElementById("border").style.display = "none"
+
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        const image = new Image();
+        image.onload = function () {
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0, image.width, image.height);
+
+            const imageData = context.getImageData(0, 0, image.width, image.height);
+            const code = jsQR(imageData.data, imageData.width, imageData.height);
+            const container = document.getElementById("parse")
+            const alert = document.getElementById("alert-text")
+            const alertIcon = document.getElementById("alert-icon")
+
+            if (!code) {
+                alert.innerText = "No QR Code Found!"
+                alertIcon.classList.add("fa-exclamation-circle")
+                alertIcon.classList.remove("fa-check-circle")
+                container.classList.add("error")
+                container.classList.remove("success")
+                document.cookie = "data-client:length=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+                document.cookie = "data-client:value=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+
+            }
+
+            let fixCode = code.data.toString().trim()
+
+            let parseData = parseQRIS(fixCode)
+            let uniQR = false
+
+            console.log(code.data.toString().trim())
+
+            for (let i = 0; i < parseData.length; i++) {
+                if (parseData[i].objectId == 51) {
+                    uniQR = true
+                    document.cookie = "data-master=" + fixCode
+                    // document.getElementById("client-qr-container").style.display = "flex"
+                    // document.getElementById("border").style.display = "block"
+                }
+            }
+
+            if (uniQR == true) {
+                alert.innerText = "Ready to merge!"
+                alertIcon.classList.add("fa-check-circle")
+                alertIcon.classList.remove("fa-exclamation-circle")
+                container.classList.add("success")
+                container.classList.remove("error")
+            }
+
+            if (uniQR == false) {
+                alert.innerText = "Unique code not found!"
+                alertIcon.classList.add("fa-exclamation-circle")
+                alertIcon.classList.remove("fa-check-circle")
+                container.classList.add("error")
+                container.classList.remove("success")
+
+            }
+        }
+        image.src = event.target.result;
+    }
+
+    reader.readAsDataURL(file);
+}
+
+document.getElementById("mergeBtn").addEventListener("click", function () {
+    if (
+        getCookie("data-master") === "" &&
+        getCookie("data-client=length") === "" &&
+        getCookie("data-client=value") === "") {
+        return 0
+    }
+    let cmasterData = getCookie("data-master")
+    let parseData = parseQRIS(cmasterData)
+
+    let data = ""
+    let title_name = ""
+    for (let i = 0; i < parseData.length; i++) {
+        const str = "" + parseData[i].objectLength
+        const pad = "00"
+
+        let value = parseData[i].objectValue
+        let length = parseData[i].objectLength
+        if (parseData[i].objectId == 51) {
+            length = getCookie("data-client:length")
+            value = getCookie("data-client:value")
+        }
+        data += parseData[i].objectId + pad.substring(0, pad.length - str.length) + length + value
+
+        if (parseData[i].objectId == 59) title_name = parseData[i].objectValue
+    }
+
+    console.log(data)
+    console.log(cmasterData)
+
+    let rawData = data
+
+    let QrUrl = "https://quickchart.io/chart?cht=qr&chs=250x250&chl=" + rawData;
+    let imgElement = document.getElementById("mergeImg");
+
+    imgElement.removeAttribute("src")
+
+    fetch(QrUrl).then(res => res.blob()).then(blob => {
+        const url = URL.createObjectURL(blob)
+        imgElement.setAttribute("src", url);
     })
+
+    let title = title_name
+    // let unique = "Test Uniques"
+    let titleholder = document.getElementById('title-placeholder-merge')
+    // let uniqueholder = document.getElementById('unique-placeholder-merge')
+    if (title != "") {
+        titleholder.innerText = title
+        titleholder.style.display = "flex"
+    }
+
+    // if (unique != "") {
+    //     uniqueholder.innerText = "NMID " + unique
+    // }
+
+    let modal = document.getElementById('modal-merge');
+    modal.style.display = 'flex';
+
+    let closeBtn = document.querySelector('.close-merge');
+
+    // Function to close the modal when clicking the close button or outside the modal
+    window.addEventListener('click', function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+            titleholder.innerText = ""
+            uniqueholder.innerText = ""
+            titleholder.style.display = "none"
+        }
+    });
+
+    closeBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+        titleholder.innerText = ""
+        uniqueholder.innerText = ""
+        titleholder.style.display = "none"
+    });
+})
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function qrClientReader() {
+    document.cookie = "data-client:length=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    document.cookie = "data-client:value=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    // document.getElementById("mergeBtn").style.display = "none"
+
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        const image = new Image();
+        image.onload = function () {
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0, image.width, image.height);
+
+            const imageData = context.getImageData(0, 0, image.width, image.height);
+            const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+            const container = document.getElementById("client-status-alert")
+            const alert = document.getElementById("alert-text-client")
+            const alertIcon = document.getElementById("alert-icon-client")
+
+            if (!code) {
+                alert.innerText = "No QR Code Found!"
+                alertIcon.classList.add("fa-exclamation-circle")
+                alertIcon.classList.remove("fa-check-circle")
+                container.classList.add("error")
+                container.classList.remove("success")
+            }
+
+            let fixData = code.data.toString().trim()
+            let parseData = parseQRIS(fixData)
+            let uniQR = false
+            for (let i = 0; i < parseData.length; i++) {
+                if (parseData[i].objectId == 51) {
+                    uniQR = true
+                    document.cookie = "data-client:length=" + parseData[i].objectLength
+                    document.cookie = "data-client:value=" + parseData[i].objectValue
+                    // document.getElementById("mergeBtn").style.display = "block"
+                }
+            }
+
+            if (uniQR == true) {
+                alert.innerText = "Ready to merge!"
+                alertIcon.classList.add("fa-check-circle")
+                alertIcon.classList.remove("fa-exclamation-circle")
+                container.classList.add("success")
+                container.classList.remove("error")
+            }
+
+            if (uniQR == false) {
+                alert.innerText = "Unique code not found!"
+                alertIcon.classList.add("fa-exclamation-circle")
+                alertIcon.classList.remove("fa-check-circle")
+                container.classList.add("error")
+                container.classList.remove("success")
+
+            }
+        }
+        image.src = event.target.result;
+    }
+
+    reader.readAsDataURL(file);
 }
 
 function copyText(el, btn) {
@@ -64,19 +300,17 @@ function getChecksum() {
     let raw = document.getElementById('rawCode').textContent
     let placeholder = document.getElementById('checksum-placeholder')
     placeholder.innerText = raw + crc16_ccitt_false(raw)
-
 }
 
+
+// Render QR Data
 document.getElementById("drawQr").addEventListener("click", function () {
     let rawData = document.getElementById('sourceCode').textContent
-    let checksum = crc16_ccitt_false(rawData)
-    // let QRParser = parseQRIS(rawData + checksum)
 
-    // Encode the data in URI format
-    var encodedData = encodeURIComponent(rawData);
-
-    var QrUrl = "https://quickchart.io/chart?cht=qr&chs=250x250&chl=" + encodedData;
+    var QrUrl = "https://quickchart.io/chart?cht=qr&chs=250x250&chl=" + rawData;
     var imgElement = document.getElementById("img");
+
+    imgElement.removeAttribute("src")
 
     fetch(QrUrl).then(res => res.blob()).then(blob => {
         const url = URL.createObjectURL(blob)
@@ -96,25 +330,10 @@ document.getElementById("drawQr").addEventListener("click", function () {
         uniqueholder.innerText = "NMID " + unique
     }
 
-    var modal = document.getElementById('modal');
+    let modal = document.getElementById('modal');
     modal.style.display = 'flex';
 
-    //Convert HTML to PNG
-    // let content = document.getElementById("qrImage")
-    // html2canvas(content).then(function (canvas) {
-    //     // Export the canvas to its data URI representation
-    //     var base64image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-
-    //     // const link = document.createElement('a');
-    //     // link.setAttribute("download", `info.png`);
-    //     // link.setAttribute("href", base64image);
-    //     // link.click();
-    //     let down = document.getElementById("download")
-    //     down.setAttribute('download', 'info.png')
-    //     down.setAttribute('href', base64image)
-    // });
-
-    var closeBtn = document.querySelector('.close');
+    let closeBtn = document.querySelector('.close');
 
     // Function to close the modal when clicking the close button or outside the modal
     window.addEventListener('click', function (event) {
@@ -139,25 +358,6 @@ function restartPage() {
     location.reload();
 }
 
-// document.getElementById("downloadBtn").addEventListener("click", function () {
-//     var imgSrc = document.getElementById("img").src;
-//     var fileName = "img.png";
-
-//     // Create a link element
-//     var downloadLink = document.createElement("a");
-//     downloadLink.href = imgSrc;
-//     downloadLink.download = fileName;
-
-//     // Append the link to the body
-//     document.body.appendChild(downloadLink);
-
-//     // Trigger the click event of the link
-//     downloadLink.click();
-
-//     // Clean up
-//     document.body.removeChild(downloadLink);
-// });
-
 function crc16_ccitt_false(data) {
     var crc = 0xFFFF;
     for (var i = 0; i < data.length; i++) {
@@ -174,22 +374,56 @@ function crc16_ccitt_false(data) {
     return crc.toString(16).toUpperCase().padStart(4, '0');
 }
 
-// function parseQRIS(qrisString) {
-//     let result = [];
-//     let currentIndex = 0;
+function parseQRIS(qrisString) {
+    let result = [];
+    let currentIndex = 0;
 
-//     while (currentIndex < qrisString.length) {
-//         const objectId = qrisString.slice(currentIndex, currentIndex + 2);
-//         currentIndex += 2;
+    while (currentIndex < qrisString.length) {
+        const objectId = qrisString.slice(currentIndex, currentIndex + 2);
+        currentIndex += 2;
 
-//         const objectLength = parseInt(qrisString.slice(currentIndex, currentIndex + 2));
-//         currentIndex += 2;
+        const objectLength = parseInt(qrisString.slice(currentIndex, currentIndex + 2));
+        currentIndex += 2;
 
-//         const objectValue = qrisString.slice(currentIndex, currentIndex + objectLength);
-//         currentIndex += objectLength;
+        const objectValue = qrisString.slice(currentIndex, currentIndex + objectLength);
+        currentIndex += objectLength;
 
-//         result.push({ objectId, objectLength, objectValue });
-//     }
+        result.push({ objectId, objectLength, objectValue });
+    }
 
-//     return result;
-// }
+    return result;
+}
+
+document.getElementById("phar").addEventListener("click", function () {
+    let data = document.getElementById("result").textContent
+    let parseData = parseQRIS(data)
+
+    let container = document.getElementById("parse")
+    if (container.firstChild) {
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild)
+        }
+    }
+
+    for (let i = 0; i < parseData.length; i++) {
+        let input_row = document.createElement("div")
+        input_row.setAttribute("class", "input-row")
+        container.appendChild(input_row)
+        let input_group = document.createElement("div")
+        input_group.setAttribute("class", "input-group")
+        input_row.appendChild(input_group)
+
+        let inputObjectId = document.createElement("input")
+        inputObjectId.setAttribute("value", parseData[i].objectId)
+        input_group.appendChild(inputObjectId)
+        const str = "" + parseData[i].objectLength
+        const pad = "00"
+        let inputObjectLength = document.createElement("input")
+        inputObjectLength.setAttribute("value", pad.substring(0, pad.length - str.length) + str)
+        input_group.appendChild(inputObjectLength)
+        let inputObjectValue = document.createElement("input")
+        inputObjectValue.setAttribute("value", parseData[i].objectValue)
+        input_group.appendChild(inputObjectValue)
+
+    }
+})
